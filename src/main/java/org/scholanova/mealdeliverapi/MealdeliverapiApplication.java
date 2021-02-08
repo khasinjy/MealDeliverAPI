50959301d764334d6710f6ebb32bee08599085af
khasinjy <k.praxele@gmail.com>
2021-02-08 15:26:41 +0100

    initial project

---
+++ src/main/java/org/scholanova/mealdeliverapi/MealdeliverapiApplication.java
@@ -0,0 +1,14 @@
+package org.scholanova.mealdeliverapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealdeliverapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealdeliverapiApplication.class, args);
+		System.out.println("group3 running");
+	}
+
+}
---
+++ src/test/java/org/scholanova/mealdeliverapi/MealdeliverapiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.scholanova.mealdeliverapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealdeliverapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
